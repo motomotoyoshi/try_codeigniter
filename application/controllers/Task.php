c2d0d48d27e43aefd04b5593e9d90b81fcfe3f4f
motomotoyoshi <motomoto.git@gmail.com>
2019-07-07 00:02:12 +0900

    create, task list

---
+++ application/controllers/Task.php
@@ -0,0 +1,24 @@
+<?php 
+
+  class Task extends CI_Controller
+  {
+    public function index()
+    {
+      $this->load->model('task_model');
+      $this->load->library('form_validation');
+
+      if ($this->input->post()){
+          $this->form_validation->set_rules('task', 'タスク', 'required|min_length[5]|max_length[20]');
+
+          if ($this->form_validation->run()){
+            $this->task_model->creates($this->input->post('task'));
+            $data['create'] = true;
+          }else{
+            $data['create'] = false;
+          }
+      }
+
+      $data['task_list'] = $this->task_model->lists();
+      $this->load->view('tutorial/task', $data);
+    }
+  }
---
+++ application/models/Task_model.php
@@ -0,0 +1,27 @@
+<?php
+
+  class Task_model extends CI_Model
+  {
+    public function __construct()
+      {
+        parent::__construct();
+        $this->load->database();
+      }
+
+      public function lists()
+      {
+        $per_page = 10; //表示数
+        $query = $this->db->get('task', $per_page);
+        if ($query->num_rows() > 0){
+          return $query->result_array();
+        }else{
+          return [];
+        }
+      }
+
+      public function creates($task)
+      {
+        $data = ['task_name' => $task];
+        $this->db->insert('task', $data);
+      }
+  }
